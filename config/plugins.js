9bd960978e2588e8bbc632ff969e66b463cd2fda
Cinquin Andy <andy.cinquin@infotel.com>
2023-08-04 17:45:45 +0200

    translation plugin done

--- config/plugins.js
+++ config/plugins.js
@@ -19,4 +19,27 @@ module.exports = ({env}) => ({
       },
     },
   },
+  translate: {
+    enabled: true,
+    config: {
+      // Choose one of the available providers
+      provider: 'deepl',
+      // Pass credentials and other options to the provider
+      providerOptions: {
+        // use custom locale mapping (for example 'en' locale is deprecated so need to choose between 'EN-GB' and 'EN-US')
+        localeMap: {
+          // use uppercase here!
+          FR: 'FR',
+          EN: 'EN-US',
+        },
+        apiOptions: {
+          // see <https://github.com/DeepLcom/deepl-node#text-translation-options> for supported options.
+          // note that tagHandling Mode cannot be set this way.
+          // use with caution, as non-default values may break translation of markdown
+          formality: 'default',
+          // ...
+        }
+      },
+    }
+  }
 });
