731964a8b9b56cde85f1ef64edc00ed198f42444
Cinquin Andy <andy.cinquin@infotel.com>
2024-01-27 17:46:28 +0100

    🔧 Update full_documentation.json and add new component types
    - Updated the "x-generation-date" in full_documentation.json to "2024-01-27T16:45:51.566Z"
    - Added new component types:
      - ContactContact
      - ContentAboutContentAbout
      - ContentBlogContentBlog
      - ContentCguContentCgu
      - ContentContactContentContact
      - ContentFooterContentFooter
      - ContentHomeContentHome
      - ContentNotfoundContentNotfound
      - ContentRealisationsContentRealisations

---
+++ types/generated/components.d.ts
@@ -0,0 +1,254 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ContactContact extends Schema.Component {
+  collectionName: 'components_contact_contacts';
+  info: {
+    displayName: 'Contact';
+    icon: 'quote';
+    description: '';
+  };
+  attributes: {
+    address: Attribute.String;
+    email: Attribute.String;
+    phone: Attribute.String;
+    langage: Attribute.String;
+  };
+}
+
+export interface ContentAboutContentAbout extends Schema.Component {
+  collectionName: 'components_content_about_content_abouts';
+  info: {
+    displayName: 'Content-about';
+    icon: 'brush';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface ContentBlogContentBlog extends Schema.Component {
+  collectionName: 'components_content_blog_content_blogs';
+  info: {
+    displayName: 'Content-blog';
+    icon: 'envelop';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface ContentCguContentCgu extends Schema.Component {
+  collectionName: 'components_content_cgu_content_cgus';
+  info: {
+    displayName: 'Content_CGU';
+    icon: 'crop';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface ContentContactContentContact extends Schema.Component {
+  collectionName: 'components_content_contact_content_contacts';
+  info: {
+    displayName: 'Content-contact';
+    icon: 'picture';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    title_name: Attribute.String;
+    title_email: Attribute.String;
+    title_phone: Attribute.String;
+    title_company: Attribute.String;
+    title_content: Attribute.String;
+    error_name: Attribute.String;
+    error_email: Attribute.String;
+    error_phone: Attribute.String;
+    error_company: Attribute.String;
+    error_content: Attribute.String;
+    informative_message: Attribute.String;
+    btn_send: Attribute.String;
+    seo: Attribute.Component<'seo.seo'>;
+    toast_error: Attribute.String;
+    toast_success: Attribute.String;
+  };
+}
+
+export interface ContentFooterContentFooter extends Schema.Component {
+  collectionName: 'components_content_footer_content_footers';
+  info: {
+    displayName: 'content_footer';
+    icon: 'server';
+    description: '';
+  };
+  attributes: {
+    image: Attribute.Media;
+    content: Attribute.RichText;
+    title_sitemap: Attribute.String;
+    title_legals: Attribute.String;
+    content_signature: Attribute.RichText;
+  };
+}
+
+export interface ContentHomeContentHome extends Schema.Component {
+  collectionName: 'components_content_home_content_homes';
+  info: {
+    displayName: 'Content_home';
+    icon: 'question';
+    description: '';
+  };
+  attributes: {
+    title_home: Attribute.String;
+    title_service: Attribute.String;
+    title_blog: Attribute.String;
+    title_realisation: Attribute.String;
+    link: Attribute.Component<'link.link', true>;
+    title_vertical_left_bottom: Attribute.String;
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface ContentNotfoundContentNotfound extends Schema.Component {
+  collectionName: 'components_content_notfound_content_notfounds';
+  info: {
+    displayName: 'Content-notfound';
+    icon: 'cup';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface ContentRealisationsContentRealisations
+  extends Schema.Component {
+  collectionName: 'components_content_realisations_content_realisations';
+  info: {
+    displayName: 'Content_realisations';
+    icon: 'crown';
+    description: '';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+    title_galery: Attribute.String;
+    title_content: Attribute.String;
+    title_technology: Attribute.String;
+    btn_galery: Attribute.Component<'link.link'>;
+    title_links: Attribute.String;
+  };
+}
+
+export interface ContentServiceContentService extends Schema.Component {
+  collectionName: 'components_content_service_content_services';
+  info: {
+    displayName: 'Content_service';
+    icon: 'alien';
+    description: '';
+  };
+  attributes: {
+    seo: Attribute.Component<'seo.seo'>;
+  };
+}
+
+export interface CtaCta extends Schema.Component {
+  collectionName: 'components_cta_ctas';
+  info: {
+    displayName: 'cta';
+    icon: 'paperPlane';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    content: Attribute.RichText;
+    link: Attribute.Component<'link.link'>;
+  };
+}
+
+export interface LinkLink extends Schema.Component {
+  collectionName: 'components_link_links';
+  info: {
+    displayName: 'Link';
+    icon: 'crown';
+  };
+  attributes: {
+    label: Attribute.String;
+    url: Attribute.String;
+  };
+}
+
+export interface MenuMenu extends Schema.Component {
+  collectionName: 'components_menu_menus';
+  info: {
+    displayName: 'Menu';
+    icon: 'book';
+    description: '';
+  };
+  attributes: {
+    Link: Attribute.Component<'link.link'>;
+  };
+}
+
+export interface SeoSeo extends Schema.Component {
+  collectionName: 'components_seo_seos';
+  info: {
+    displayName: 'Seo';
+    icon: 'quote';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.String;
+    h1: Attribute.String;
+    canonical: Attribute.String;
+  };
+}
+
+export interface SitemapsSitemaps extends Schema.Component {
+  collectionName: 'components_sitemaps_sitemaps';
+  info: {
+    displayName: 'Sitemaps';
+    icon: 'crown';
+  };
+  attributes: {
+    Link: Attribute.Component<'link.link'>;
+    categorie: Attribute.String;
+  };
+}
+
+export interface SocialsSocials extends Schema.Component {
+  collectionName: 'components_socials_socials';
+  info: {
+    displayName: 'socials';
+    icon: 'chartCircle';
+  };
+  attributes: {
+    linkedin: Attribute.String;
+    facebook: Attribute.String;
+    instagram: Attribute.String;
+    github: Attribute.String;
+    malt: Attribute.String;
+    email: Attribute.String;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'contact.contact': ContactContact;
+      'content-about.content-about': ContentAboutContentAbout;
+      'content-blog.content-blog': ContentBlogContentBlog;
+      'content-cgu.content-cgu': ContentCguContentCgu;
+      'content-contact.content-contact': ContentContactContentContact;
+      'content-footer.content-footer': ContentFooterContentFooter;
+      'content-home.content-home': ContentHomeContentHome;
+      'content-notfound.content-notfound': ContentNotfoundContentNotfound;
+      'content-realisations.content-realisations': ContentRealisationsContentRealisations;
+      'content-service.content-service': ContentServiceContentService;
+      'cta.cta': CtaCta;
+      'link.link': LinkLink;
+      'menu.menu': MenuMenu;
+      'seo.seo': SeoSeo;
+      'sitemaps.sitemaps': SitemapsSitemaps;
+      'socials.socials': SocialsSocials;
+    }
+  }
+}
