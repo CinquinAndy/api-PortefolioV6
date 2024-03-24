5d8c6aff8be61b369b8f5a37b37d124c60715e24
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-24 20:46:28 +0100

    ✨ Update gitignore and middlewares configuration
    - Updated .gitignore to exclude additional files and directories.
    - Modified CORS settings in middlewares for Strapi security.

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,29 +1,33 @@
-module.exports = ({ env }) => {
-  var ssl = env("S3_SSL", "false") === "true" ? "https://" : "http://";
-  const s3url = new URL(ssl + env("S3_ENDPOINT"));
-  return [
-    "strapi::errors",
-    {
-      name: "strapi::security",
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            "connect-src": ["'self'", "https:"],
-            "img-src": ["'self'", "data:", "blob:", s3url.host],
-            "media-src": ["'self'", "data:", "blob:", s3url.host],
-            upgradeInsecureRequests: null,
-          },
+module.exports = ({env}) => {
+    var ssl = env("S3_SSL", "false") === "true" ? "https://" : "http://";
+    const s3url = new URL(ssl + env("S3_ENDPOINT"));
+    return [
+        "strapi::errors",
+        {
+            name: "strapi::security",
+            config: {
+                cors: {
+                    origin: ['*'], //allow all origins
+                    headers: ['*'], //allow all headers
+                },
+                contentSecurityPolicy: {
+                    useDefaults: true,
+                    directives: {
+                        "connect-src": ["'self'", "https:"],
+                        "img-src": ["'self'", "data:", "blob:", s3url.host],
+                        "media-src": ["'self'", "data:", "blob:", s3url.host],
+                        upgradeInsecureRequests: null,
+                    },
+                },
+            },
         },
-      },
-    },
-    "strapi::cors",
-    "strapi::poweredBy",
-    "strapi::logger",
-    "strapi::query",
-    "strapi::body",
-    "strapi::session",
-    "strapi::favicon",
-    "strapi::public",
-  ];
+        "strapi::cors",
+        "strapi::poweredBy",
+        "strapi::logger",
+        "strapi::query",
+        "strapi::body",
+        "strapi::session",
+        "strapi::favicon",
+        "strapi::public",
+    ];
 };
